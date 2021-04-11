5d429243758cf67856a8a4abb38b73499920cd4d
肖坤 <13470106153@qq.com>
2021-04-11 16:09:28 +0800

    肖坤4.11

--- src/view/no1_3.js
+++ src/view/no1_3.js
@@ -1,20 +1,111 @@
 import {Switch,Route,Link} from 'react-router-dom' 
+import { Modal, Button,Tabs,Upload, message} from 'antd';
+import { PaperClipOutlined } from '@ant-design/icons';
 import No1_3_1 from './no1_3_1'
 import No1_3_2 from './no1_3_2'
+import './no1_3.css'
+import {useState} from 'react'
+
 
 
 function App(){
+    const props = {
+        name: 'file',
+        action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+        headers: {
+          authorization: 'authorization-text',
+        },
+        onChange(info) {
+          if (info.file.status !== 'uploading') {
+            console.log(info.file, info.fileList);
+          }
+          if (info.file.status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully`);
+          } else if (info.file.status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+          }
+        },
+      };
+    const { TabPane } = Tabs;
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+  function showModal(){
+    setIsModalVisible(true);
+  };
+
+  function handleOk(){
+    setIsModalVisible(false);
+  };
+
+  function handleCancel(){
+    setIsModalVisible(false);
+  };
     return(
-        <div>
-            <div><Link to="/App/no1_3/">31</Link></div>
-            <div><Link to="/App/no1_3/no1_3_2">32</Link></div>
-            
-            <div>
-               <Switch> 
-                    <Route path='/APP/no1_3/no1_3_2' component={No1_3_2}></Route>
-                    <Route path='/APP/no1_3' component={No1_3_1}></Route>
-                </Switch>
+        <div className="no1_3_nav">
+            <div className="no1_3_nav_1">
+                <div className="no1_3_title">
+                    <div>
+                        <Tabs defaultActiveKey="2">
+                            <TabPane tab={<span><Link to="/App/no1_3/">我的任务</Link></span>}key="1">
+                                
+                            </TabPane>
+                            <TabPane tab={<span><Link to="/App/no1_3/no1_3_2">我下属的任务</Link></span>}key="2">
+                                
+                            </TabPane>
+                        </Tabs>
+                    </div>
+                    <div className="no1_3_title_2">
+                        <Button type="primary" onClick={showModal}>
+                            新建任务
+                        </Button>
+                        <Modal className="no1_3_Modal" title="新建任务" okText="确定" cancelText="取消" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} width={1000} maskStyle={{background:'#ddd'}}>
+                            <ul className="no1_3_Modal_1">
+                                <li>
+                                    <h3>* 任务名称</h3>
+                                    <input type="text" placeholder="请输入内容" />
+                                </li>
+                                <li>
+                                    <h3>负责人</h3>
+                                    <input type="text" placeholder="请输入内容" />
+                                </li>
+                                <li>
+                                    <h3>开始时间</h3>
+                                    <input type="text" placeholder="请输入内容"/>
+                                </li>
+                                <li>
+                                    <h3>结束时间</h3>
+                                    <input type="text" placeholder="请输入内容"/>
+                                </li>
+                            </ul>
+                            <div className="no1_3_Modal_2">
+                                <h3>优先级</h3>
+                                <div>高</div>
+                                <div>中</div>
+                                <div>低</div>
+                                <div>无</div>
+                            </div>
+                            <div className="no1_3_Modal_3">
+                                <h3>任务描述</h3>
+                                <textarea autoComplete="off" placeholder="请输入内容"/>
+                            </div>
+                            <div className="no1_3_Modal_4">
+                                <Upload {...props}>
+                                    <Button icon={<PaperClipOutlined />}>关联业务</Button>
+                                </Upload>
+                            </div>
+                        </Modal>
+                    </div>
+                </div>
+                <div>
+                    <div className="no1_3_content">
+                        <Switch> 
+                            <Route path='/APP/no1_3/no1_3_2' component={No1_3_2}></Route>
+                            <Route path='/APP/no1_3' component={No1_3_1}></Route>
+                        </Switch>
+                    </div>
+                </div>
             </div>
+            
         </div>
     )
 }
--- src/view/no1_3_2.js
+++ src/view/no1_3_2.js
@@ -1,7 +1,137 @@
+import './no1_3_2.css'
+import { Cascader } from 'antd';
 function App(){
+    const type=false
+    const options = [
+        {
+            code: '全部',
+            name: '全部',
+        },
+        {
+            code: '我负责的',
+            name: '我负责的',
+        },
+        {
+            code: '我创建的',
+            name: '我创建的',
+        },
+        {
+            code: '我参与的',
+            name: '我参与的',
+        },
+    ]
+    const options2 = [
+        {
+            code: '全部',
+            name: '全部',
+        },
+        {
+            code: '正在进行',
+            name: '正在进行',
+        },
+        {
+            code: '已完成',
+            name: '已完成',
+        },
+    ]
+    const options3 = [
+        {
+            code: '全部',
+            name: '全部',
+        },
+        {
+            code: '高',
+            name: '高',
+        },
+        {
+            code: '中',
+            name: '中',
+        },
+        {
+            code: '低',
+            name: '低',
+        },
+        {
+            code: '无',
+            name: '无',
+        },
+    ]
+    const options4 = [
+        {
+            code: '全部',
+            name: '全部',
+        },
+        {
+            code: '今天到期',
+            name: '今天到期',
+        },
+        {
+            code: '明天到期',
+            name: '明天到期',
+        },
+        {
+            code: '一周到期',
+            name: '一周到期',
+        },
+        {
+            code: '一个月到期',
+            name: '一个月到期',
+        },
+    ]
+    const options5 = [
+        {
+            code: '全部',
+            name: '全部',
+        },
+        {
+            code: '今天到期',
+            name: '今天到期',
+        },
+        {
+            code: '明天到期',
+            name: '明天到期',
+        },
+        {
+            code: '一周到期',
+            name: '一周到期',
+        },
+        {
+            code: '一个月到期',
+            name: '一个月到期',
+        },
+    ]
     return(
-        <div>
-            工作台32
+        <div>   
+            <div className="no1_3_2_title">
+                <input type="text" placeholder="搜索人物名称" className="no1_3_2_input"></input>
+                <div>
+                <ul>
+                    <li>任务类型</li>
+                    <li>
+                    <Cascader allowClear={type} fieldNames={{ label: 'name', value: 'code'}} options={options} placeholder="全部"/>
+                    </li>
+                    <li>状态</li>
+                    <li>
+                        <Cascader allowClear={type} fieldNames={{ label: 'name', value: 'code'}} options={options2} placeholder="全部"/>
+                    </li>
+                    <li>优先级</li>
+                    <li>
+                        <Cascader allowClear={type} fieldNames={{ label: 'name', value: 'code'}} options={options3} placeholder="全部"/>
+                    </li>
+                    <li>截止时间</li>
+                    <li>
+                        <Cascader allowClear={type} fieldNames={{ label: 'name', value: 'code'}} options={options4} placeholder="全部"/>
+                    </li>
+                    <li>负责人</li>
+                    <li>
+                        <Cascader allowClear={type} fieldNames={{ label: 'name', value: 'code'}} options={options5} placeholder="全部"/>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            <div className="no1_3_2_content">
+                 <p>没有更多了</p>
+            </div>
         </div>
     )
 }
