704aaebcbd07da4b569bface7f6fb9e3bd9667f2
刘帅 <676514266@qq>
2021-04-11 15:05:25 +0800

    刘帅

--- src/view/no1_2.js
+++ src/view/no1_2.js
@@ -1,7 +1,72 @@
-function App(){
-    return(
-        <div>
-            工作台2
+
+
+import { Calendar, Alert } from 'antd';  //组件1
+import moment from 'moment';
+
+import React, { useState } from 'react'; //组件2
+import { Modal, Button } from 'antd';
+
+import { DatePicker, Space } from 'antd'; //组件3 小日历
+
+
+
+import './no1_2.css'
+moment.locale('zh-cn')
+
+function onPanelChange(value, mode) {
+    console.log(value.format('YYYY-MM-DD'), mode);
+}
+function App() {
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    function showModal() {
+        setIsModalVisible(true);
+    };
+
+    function handleOk() {
+        setIsModalVisible(false);
+    };
+
+    function handleCancel() {
+        setIsModalVisible(false);
+    };
+    const { RangePicker } = DatePicker;
+    return (
+
+        <div className="no1_2_box">
+
+            <div className="no1_1_nav">   {/* 导航部分 */}
+                <div className="no1_1_navtop">
+                    <div>
+                        <button className="navtop_left">今天</button>
+                        <button className="navtop_rigth">日</button>
+                        <button className="navtop_rigth">周</button>
+                        <button className="navtop_rigth bg">月</button>
+                    </div>
+                </div>
+                <div className="nav_center">
+                    <span className="iconfont zs"><button>&#xe605;</button></span>
+                    <h2 className="rq">2019年 四月</h2>
+                    <span className="iconfont zz"><button>&#xe647;</button></span>
+                </div>
+                <div className="nav_rigth"> {/*创建日程*/}
+                    <Button type="primary" onClick={showModal}>
+                        创建日程
+                    </Button>
+                    <Modal title="Basic Modal" title="创建日程" okText="保存" cancelText="取消" width={800} maskStyle={{ background: '#ddd' }} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                        <p>*主题  <input type="text" className="ass acc"></input></p><br/>
+                        <p>*参与人  <input type="text" className="ass app"></input></p><br/>
+                        <p>备注 </p>
+                        <textarea autocomplete="off" className="no2_2_axx" placeholder="请输入内容"></textarea>
+                    </Modal>
+                </div>
+            </div>
+
+            <div className="no1_1_content"> {/*  内容 */}
+                <Calendar onPanelChange={onPanelChange} />   {/*ant此组件功能插入不进去*/}
+
+            </div>
         </div>
     )
 }
